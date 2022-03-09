6cf2eed0ba49d5f766f7b4f4bd0a07d3eecf7f28
Egon <loliegon@gmail.com>
2022-03-09 13:25:55 +0100

    finished fetching

--- src/05_fetch.js
+++ src/05_fetch.js
@@ -10,13 +10,51 @@ const NicosRepositories = `https://api.github.com/users/minimumviableperson/repo
 //  - only keep the .name property of each repository.
 //  - return all Nico's repo names as an array of strings.
 
+const getNamesOfNicosRepos = () => {
+    return fetch(`https://api.github.com/users/minimumviableperson/repos`)
+        .then(resp => resp.json())
+        .then(repos => repos.map(repo => repo.name))
+        .catch(error => console.warn(error));
+}
+
 // 2. Create a JSON server. Add a /posts endpoint to it, which returns a list of posts.
 // A post is: {
 //   id: number,
 //   title: string,
 //   text: string
+
+fetch('http://localhost:3004/posts', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        title: "This might be marwellous",
+        text: "Here I am, you are not alone!"
+    })
+})
+
 // }
 // 2.1 Write a function called createPost that:
 // - takes a post as an argument
 // - save your post in the JSON server
 // - console.log the newly created post from the response (it must contain the new post's id);
+
+const Post = {
+    title: "Function post",
+    text: "I'm creating this post inside a function"
+}
+
+function createPost(Post) {
+    return fetch('http://localhost:3004/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: Post.title,
+            text: Post.text
+        })
+    })
+}
+createPost(Post)
